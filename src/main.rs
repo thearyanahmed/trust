4f9ba4e71c300b2d9fa85256664f9c45ec0e9b59
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2024-01-09 22:07:05 +0100

    Add tunio

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,55 @@
-extern crate tun;
+#[allow(dead_code)]
+use std::io;
 
-use std::io::{self, Read};
+use std::io::{Read, Write};
+// use tunio::platform::utun::Driver;
+use tunio::traits::{DriverT, InterfaceT};
+use tunio::{DefaultDriver, DefaultInterface};
 
 fn main() -> io::Result<()> {
-    let mut config = tun::Configuration::default();
-    config
-        .address((10, 0, 0, 1))
-        .netmask((255, 255, 255, 0))
-        .up();
+    let _ = trust();
+    // tunio();
 
-    let mut dev = tun::create(&config).unwrap();
-    let mut buf = [0; 4096];
+    Ok(())
+}
+fn tunio() {
+    // DefaultDriver is an alias for a supported driver for current platform.
+    // It may be not optimal for your needs (for example, it can lack support of TAP),
+    // but it will work in some cases. If you need another driver, then import and use it instead.
+    let mut driver = DefaultDriver::new().unwrap();
+    // Preparing configuration for new interface. We use `Builder` pattern for this.
+    let if_config = DefaultDriver::if_config_builder()
+        .name("tun0".to_string())
+        .build()
+        .unwrap();
+
+    // Then, we create the interface using config and start it immediately.
+    let mut interface =
+        DefaultInterface::new_up(&mut driver, if_config).expect("failed to create interface");
+
+    // The interface is created and running.
+
+    // Write to interface using Write trait
+    let buf = [0u8; 4096];
+    let _ = interface.write(&buf);
+
+    // Read from interface using Read trait
+    let mut mut_buf = [0u8; 4096];
+    let _ = interface.read(&mut mut_buf);
+}
+
+fn trust() -> io::Result<()> {
+    println!("Init");
+
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+
+    let mut buf = [0u8; 1504];
+
+    eprintln!("entering loop, check tun0 dev in another devices");
 
     loop {
-        let amount = dev.read(&mut buf).unwrap();
-        println!("something is here{:?}", &buf[0..amount]);
+        let read_bytes = nic.recv(&mut buf[..])?;
+
+        eprintln!("read {} and bytes {:x?}", read_bytes, &buf[..read_bytes]);
     }
 }
